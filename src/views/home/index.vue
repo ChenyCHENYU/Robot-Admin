8ddb1cbc8c7aa605ff7a0435e9745bd76d8af8e2
ChenYu <ycyplus@163.com>
2022-11-11 16:50:42 +0800

    fix(custom): 修复i18n插件无效的问题

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,7 +2,7 @@
  * @Author: ChenYu
  * @Date: 2022-03-04 00:28:53
  * @LastEditors: ChenYu ycyplus@163.com
- * @LastEditTime: 2022-11-11 15:32:22
+ * @LastEditTime: 2022-11-11 16:50:12
  * @FilePath: \vue3_vite3_elementPlus_admin\src\views\home\index.vue
  * @Description: 
  * Copyright (c) ${2022} by ChenYu/天智AgileTeam, All Rights Reserved. 
@@ -17,7 +17,6 @@
     <el-button type="info">Info</el-button>
     <el-button type="warning">Warning</el-button>
     <el-button type="danger">Danger</el-button>
-    <el-button>中文</el-button>
   </el-row>
 </template>
 
