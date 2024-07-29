92a8e61def80864d40736aab3d83f2a947508ac0
ChenYu <ycyplus@gmail.com>
2024-07-29 16:22:29 +0800

    chore(custom): 更新apifox云端接口url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: ChenYu
  * @Date: 2022-03-03 23:38:18
  * @LastEditors: ChenYu ycyplus@gmail.com
- * @LastEditTime: 2024-07-29 15:58:35
+ * @LastEditTime: 2024-07-29 16:21:32
  * @FilePath: \vue3_vite3_element-plus_admin\vite.config.ts
  * @Description: vite 配置文件
  * Copyright (c) ${2022} by ChenYu/天智AgileTeam, All Rights Reserved.
@@ -101,7 +101,7 @@ export default defineConfig(({ mode }) => {
       open: true,
       proxy: {
         '^/api': {
-          // target: 'http://127.0.0.1:4523/m1/4902805-4559325-default', // APIFOX 代理接口
+          // target: 'http://127.0.0.1:4523/m1/4902805-4559325-default', // APIFOX 本地代理接口
           target: 'https://apifoxmock.com/m1/4902805-4559325-default', // APIFOX 代理接口
           // target: 'http://121.89.210.252:3000/mock/2228/', // YAPI代理接口
           changeOrigin: true,
