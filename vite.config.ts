b8a24054fb8d0b74a0b02fb9028e33f9b0287663
ChenYu <ycyplus@gmail.com>
2024-08-05 18:08:07 +0800

    test(custom): 调试apifox云端MockUrl

--- vite.config.ts
+++ vite.config.ts
@@ -102,7 +102,7 @@ export default defineConfig(({ mode }) => {
       proxy: {
         '^/api': {
           // target: 'http://127.0.0.1:4523/m1/4902805-4559325-default', // APIFOX 本地代理接口
-          target: 'https://www.robotadmin.cn', // APIFOX 代理接口
+          target: 'https://apifoxmock.com/m1/4902805-4559325-default', // APIFOX 代理
           // target: 'http://121.89.210.252:3000/mock/2228/', // YAPI代理接口
           changeOrigin: true,
           rewrite: path => path.replace(/^\/api/, ''),
